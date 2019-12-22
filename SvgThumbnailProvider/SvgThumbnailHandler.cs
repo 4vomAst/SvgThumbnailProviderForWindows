18e0fac00ea60d7802f1c373796b0eb23b6516c3
Wolfgang <wolfgang@wallhaeuser.eu>
2019-12-22 16:23:20 +0100

    Initial commit

---
+++ SvgThumbnailProvider/SvgThumbnailHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using SharpShell.Attributes;
+using SharpShell.SharpThumbnailHandler;
+using SharpShell.SharpContextMenu;
+using Svg;
+using System.Windows.Forms;
+
+namespace SvgThumbnailProvider
+{
+   [ComVisible(true)]
+   [COMServerAssociation(AssociationType.ClassOfExtension, ".svg")]
+   public class SvgThumbnailHandler : SharpThumbnailHandler
+   {
+      public SvgThumbnailHandler()
+      {
+         Log("Constructor");
+      }
+
+      protected override Bitmap GetThumbnailImage(uint width)
+      {
+         Log("Drin");
+
+         //  Create a stream reader for the selected item stream
+         try
+         {
+            var svgDoc = SvgDocument.Open<SvgDocument>(SelectedItemStream);
+
+            return  svgDoc.Draw((int)width, (int)width);
+         }
+         catch (Exception exception)
+         {
+            //  Log the exception and return null for failure
+            LogError("An exception occurred opening the text file.", exception);
+            return null;
+         }
+      }
+
+   }
+
+
+}
